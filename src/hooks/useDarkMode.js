9c326f89aa1bf688a448564e226d3d7892090e37
Jason Sonnichsen <Harliquinn@gmail.com>
2019-09-04 16:26:26 -0400

    Yep, that worked

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import {useLocalStorage} from "./useLocalStorage";
 
-export const useDarkMode = () => {
-    const [value,setValue]=useLocalStorage('darkMode',false);
+export const useDarkMode = (initState) => {
+    const [value,setValue]=useLocalStorage('darkMode',initState);
     useEffect(()=>{
         if (value){
             if (!document.body.classList.contains('dark-mode')){document.body.classList.add('dark-mode');};
