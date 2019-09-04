03ee21c5111a09ee244bce8a1d3a603d289a7184
Jason Sonnichsen <Harliquinn@gmail.com>
2019-09-04 18:44:41 -0400

    Styled nav links and changed color in dark mode so they can be seen now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = (props) => {
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
-      <Link to="/" onClick={()=>props.cb("/")}>Top 10 Coins</Link>
-      <Link to="/doge" onClick={()=>props.cb("/doge")}>Dogecoin</Link>
-      <Link to="/abyss" onClick={()=>props.cb("/abyss")}>Abyss</Link>
-      <Link to="/mithril" onClick={()=>props.cb("/mithril")}>Mithril</Link>
-      <Link to="/bitcoen" onClick={()=>props.cb("/bitcoen")}>Bitcoen</Link>
+      <Link to="/" onClick={()=>props.cb("/")} className="navLink">Top 10 Coins</Link>
+      <Link to="/doge" onClick={()=>props.cb("/doge")} className="navLink">Dogecoin</Link>
+      <Link to="/abyss" onClick={()=>props.cb("/abyss")} className="navLink">Abyss</Link>
+      <Link to="/mithril" onClick={()=>props.cb("/mithril")} className="navLink">Mithril</Link>
+      <Link to="/bitcoen" onClick={()=>props.cb("/bitcoen")} className="navLink">Bitcoen</Link>
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -6,8 +6,17 @@ export const useDarkMode = (initState) => {
     useEffect(()=>{
         if (value){
             if (!document.body.classList.contains('dark-mode')){document.body.classList.add('dark-mode');};
+            const linkSelector=document.querySelectorAll(".navLink");
+            linkSelector.forEach(item=>{
+                item.classList.add("navLinkDark");
+            });
+            // console.log(linkSelector);
         } else {
             if (document.body.classList.contains('dark-mode')){document.body.classList.remove('dark-mode');};
+            const linkSelector=document.querySelectorAll(".navLink");
+            linkSelector.forEach(item=>{
+                item.classList.remove("navLinkDark");
+            });
         }
     },[value])
 
